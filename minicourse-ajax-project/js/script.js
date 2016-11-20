6d59222563daf1d282edfec1bc42f6409cd616d0
Daniel Escobar <escobard90@gmail.com>
2016-11-20 12:50:09 -0700

    type: doc
    added documentation for jQuery variables, and AJAX

--- minicourse-ajax-project/js/script.js
+++ minicourse-ajax-project/js/script.js
@@ -1,6 +1,12 @@
 
 function loadData() {
 
+    // keep in mind that using libraries to simplify the code you have to write is THE way to develop
+    // sets the jquery objects 
+    // Dollar signs are unecessary; for the sake of this course we are using them to identify objects as jquery objects
+    // will be using the .ajax method and the .getJSON method
+    // .ajax can be revised here : http://api.jquery.com/jquery.ajax/
+    //  .JSOn can be revised here : http://api.jquery.com/jquery.getjson/
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
